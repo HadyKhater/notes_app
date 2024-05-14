1023dda1ac2b252dcd9c3bce3348fec3baeeee01
hady Khater <hadyhanykhater@gmail.com>
2024-05-15 00:08:59 +0300

    create Notes Item

--- lib/views/widget/notes_view_body.dart
+++ lib/views/widget/notes_view_body.dart
@@ -3,39 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'custom_app_bar.dart';
 import 'custom_notes_item.dart';
+import 'notes_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
-          const SizedBox(
+          SizedBox(
             height: 50,
           ),
-          const CustomAppBar(),
-          const SizedBox(
-            height: 15.0,
-          ),
-          // Expanded(
-          //   child: ListView.separated(
-          //     itemBuilder: (context, index) {
-          //       return const CustomNotesItem();
-          //     },
-          //     separatorBuilder: (context, index) {
-          //       return const SizedBox(
-          //         height: 10.0,
-          //       );
-          //     },
-          //     itemCount: 5,
-          //   ),
-          // ),
-          CustomNotesItem()
+          CustomAppBar(),
+          Expanded(child: NotesItem()),
         ],
       ),
     );
   }
 }
+
+
