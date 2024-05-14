6036b11c5eef53ab935b000f742e835b297fa771
hady Khater <hadyhanykhater@gmail.com>
2024-05-14 20:41:03 +0300

    create  app bar it contains text and icon search

---
+++ lib/views/widget/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46.0,
+      width: 46.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.0),
+        color: Colors.white.withOpacity(.05),
+      ),
+      child: Center(
+        child: Icon(
+          Icons.search,size: 24,
+        ),
+      ),
+    );
+  }
+}
