6a4834419319a8dc2b2732659dd59aa8ad9f6192
hady Khater <hadyhanykhater@gmail.com>
2024-05-18 19:52:14 +0300

    add custom text filed only

--- lib/views/widget/add_note_buttom_sheet.dart
+++ lib/views/widget/add_note_buttom_sheet.dart
@@ -1,36 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
+import 'custom_text_filed.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child:Column(
         children: [
-          const SizedBox(
+          SizedBox(
             height: 32,
           ),
-          TextField(
-            cursorColor: kPrimaryColor,
-            decoration: InputDecoration(
-              hintText: 'Title',
-              border: buildBorder(),
-              enabledBorder: buildBorder(),
-              focusedBorder: buildBorder(kPrimaryColor),
-            ),
-          ),
+          CustomTextFiled(),
         ],
       ),
     );
   }
 }
 
-OutlineInputBorder buildBorder([color]) {
-  return OutlineInputBorder(
-    borderRadius: BorderRadius.circular(8.0),
-    borderSide: BorderSide(color: color ?? Colors.white),
-  );
-}
+
