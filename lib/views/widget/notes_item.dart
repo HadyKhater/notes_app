4ce5183eb1444d6b5a2cc5e27adee410f20b89b8
hady Khater <hadyhanykhater@gmail.com>
2024-05-18 21:27:47 +0300

    create edit note view

--- lib/views/widget/notes_item.dart
+++ lib/views/widget/notes_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 import 'custom_notes_item.dart';
 
 class NotesItem extends StatelessWidget {
@@ -8,17 +9,29 @@ class NotesItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ListView.separated(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return const CustomNotesItem();
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(
-            height: 10.0,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const EditNoteView();
+              },
+            ),
           );
         },
-        itemCount: 5,
+        child: ListView.separated(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return const CustomNotesItem();
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(
+              height: 10.0,
+            );
+          },
+          itemCount: 5,
+        ),
       ),
     );
   }
